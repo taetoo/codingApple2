3ec282e42b2b7584b9b98172cdecc890145c1c0f
taetoo <rlaougs5@naver.com>
2023-01-13 15:51:57 +0900

    Add: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+/* eslint-disable */ 
+
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import { useState } from 'react';
+// import coke from './assets/coke.jpeg'
+// import hatban from './assets/hatban.jpeg'
+// import udong from './assets/udong.jpeg'
+
+import data from './data.js';
+
 
 function App() {
+  let [merchandise] = useState(data)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand className="logo" href="#home">TaeToo's Market</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#Shop">Shop</Nav.Link>
+            </Nav>
+          </Container>
+      </Navbar>
+      <img className='main-image' src={process.env.PUBLIC_URL + '/assets/해변.jpeg'} width='100%'></img>
+      <div className="container">
+        <div className="row">
+          <Merchan merchandise={merchandise}/>
+          <Merchan merchandise={merchandise}/>
+          <Merchan merchandise={merchandise}/>
+        </div>
+      </div>
     </div>
   );
 }
+function Merchan(props) {
+  return(
+    <div className="col-md-4">
+      <img src={process.env.PUBLIC_URL + '/assets/coke.jpeg'} width="80%"/>
+      <h4>{props.merchandise[0].title}</h4>
+      <p>{props.merchandise[0].content}</p>
+      <p>{props.merchandise[0].price}</p>
+    </div>
+  )
+}  
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+      id : 0,
+      title : "Coka cola",
+      content : "Born in Atlanta",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "HatBan",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Udong",
+      content : "Born in Seoul",
+      price : 130000
+    }
+]
+
+export default data;
